6424a5db6199e5e29772f5c8f0d8be0b60ce9cc0
zulker0010 <zulkeryusuf@gmail.com>
2022-08-09 15:08:28 +0600

    changes

--- loginforms/src/App.js
+++ loginforms/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/header';
 import RegistrationForm from './components/registrationForms';
 
+
+
 function App() {
   return (
     <div className="App">
