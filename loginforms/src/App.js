0c22dcaceabf1db9c7b911f0f19c4bc52e6356b3
zulker0010 <zulkeryusuf@gmail.com>
2022-08-09 11:45:20 +0600

    changed css

--- loginforms/src/App.js
+++ loginforms/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import RegistrationForm from './components/registrationForms';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <RegistrationForm/>
     </div>
   );
 }
